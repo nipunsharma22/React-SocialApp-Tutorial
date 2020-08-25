ac6b2b252948af216d1351073eb7dd8d8221831f
Nipun <Nipun.Sharma@rsystems.com>
2020-08-25 15:47:34 +0530

    Till Loading icon

---
+++ app/components/ProfilePosts.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react"
+import Axios from "axios"
+import { useParams, Link } from "react-router-dom"
+import LoadingDotsIcon from "./LoadingDotsIcon"
+
+function ProfilePosts() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [posts, setPosts] = useState([])
+  const { username } = useParams()
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await Axios.get(`/profile/${username}/posts`)
+        console.log(response.data)
+        setPosts(response.data)
+        setIsLoading(false)
+      } catch (e) {
+        console.log("There was some problem!")
+      }
+    }
+    fetchPosts()
+  }, [])
+  if (isLoading) return <LoadingDotsIcon />
+  return (
+    <div className="list-group">
+      {posts.map(post => {
+        const date = new Date(post.createdDate)
+        const dateFormatted = `${
+          date.getMonth() + 1
+        }/${date.getDate()}/${date.getFullYear()}`
+        return (
+          <Link
+            key={post._id}
+            to={`/post/${post._id}`}
+            className="list-group-item list-group-item-action"
+          >
+            <img className="avatar-tiny" src={post.author.avatar} />{" "}
+            <strong>{post.title}</strong>{" "}
+            <span className="text-muted small">on {dateFormatted}</span>
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+export default ProfilePosts
